c2e165b25c4f1cb05b57e9f79d3c29867551f5fa
Maurice Heine <maurice@ling-app-com>
2024-08-21 16:45:01 +0200

    added search button and result list

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,26 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import { Button, SearchBar } from '@rneui/themed';
+import { Alert } from "react-native";
 
 export default function Index() {
+
+   // temp until i added redux
+   const [searchQuery, setSearchQuery] = useState("");
+   const [searched, setSearched] = useState("");
+
+
+   let updateSearch = (search: any) => {
+     // do the searching
+     setSearchQuery(search)
+   };
+
+   let renderIt = (item : string) =>{
+    return (<View>
+      <Text>{item}</Text>
+    </View>)
+   }
+ 
   return (
     <View
       style={{
@@ -9,7 +29,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <SearchBar 
+        placeholder="search user" 
+        onChangeText={updateSearch}
+        value={searchQuery}
+        />
+        <Button title="Search" onPress={()=> setSearched(searchQuery)}></Button>
+      <Text>Last search was: "{searched}"</Text>
+      <FlatList data={["user1", "user2", "user3", "user3"]} renderItem={({item, index}) => renderIt(item) }></FlatList>
     </View>
   );
 }
